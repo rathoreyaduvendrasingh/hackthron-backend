77af24c24c9838329fe91ed0536d2d4ba57c7df8
techdestroyeryadu <66662616+techdestroyeryadu@users.noreply.github.com>
2022-09-17 16:19:13 +0530

    new url

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'https://hackthron-blog.netlify.app/'
+      'https://bloggers-demo.netlify.app/'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
